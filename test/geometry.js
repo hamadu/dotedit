40b00dbcb2cb0c1c2c88abd55faa6c55b01cd1e9
hamadu <dercll1020@gmail.com>
2017-05-30 18:24:56 +0900

    let run the test with "npm test"

--- test/geometry.js
+++ test/geometry.js
@@ -3,12 +3,12 @@ import Geometry from "../src/geometry";
 
 describe('lineToDots', function () {
   it('computes line correctly', function () {
-    assert.deepEqual(Geometry.lineToDots(0, 1, 0, 3), [[0, 1], [0, 2], [0, 3]]);
+    assert.deepEqual(Geometry.line(0, 1, 0, 3), [[0, 1], [0, 2], [0, 3]]);
   });
 });
 
 describe('ovalToDots', function () {
   it('computes oval correctly', function () {
-    assert.deepEqual(Geometry.ovalToDots(0, 0, 0, 0), []);
+    assert.deepEqual(Geometry.oval(0, 0, 0, 0), [[0, 0]]);
   });
 });
